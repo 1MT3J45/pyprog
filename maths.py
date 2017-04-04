f9750436d2c9aed0fa1a858df9e67be3f95a7052
1MT3J45 <agenttj.rawal98@gmail.com>
2017-04-05 01:41:48 +0530

    From my Practice

--- maths.py
+++ maths.py
@@ -1,11 +1,34 @@
-from math import *
+import random
+import sys
+import os
 
-a = input("Enter the number: ")
-b = sqrt(a)
+print("learning arithmetics in python")
 
-print "Square root is: ", b
-print "Log is : ", log(a)
-print "Tan : ", tan(a)
-print "Value of PI:", pi
-print "Value of e:", e
+print("ADDITION")
+print("5 + 2=", 5+2)
+
+print("\n SUBTRACTION")
+print("5 - 2=", 5-2)
+
+print("\n MULTIPLICATION")
+print("5 * 2=", 5*2)
+
+print("\n DIVISION")
+print("5 / 2=", 5/2)
+
+print("\n MODULUS")
+print("5 % 2=", 5%2)
+
+print("\n EXPONENTIAL")
+print("5 ** 2=", 5**2)
+
+print("\n DIVISION PURGE REMAINDER & ROUNDOFF")
+print("5 // 2=", 5//2)
+
+# LEARNING FLOW OF OPERATIONS
+
+print("\n\tWith and Without Brackets - Intro to BODMAS")
+
+print("1 + 2 - 3 * 2 =", 1 + 2 - 3 * 2)
+print("\n(1 + 2 - 3) *2 =", (1 + 2 -3)*2)
 
